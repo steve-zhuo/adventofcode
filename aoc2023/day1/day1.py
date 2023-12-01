bdb70421f877ed68b404499d89949a9085e1cb15
Steve Zhuo <wqzhuo@gmail.com>
2023-12-01 12:32:27 -0500

    day1 done

--- aoc2023/day1/day1.py
+++ aoc2023/day1/day1.py
@@ -7,40 +7,58 @@ class Calibration:
             return 0
         else:
             return int(self.a[0] + self.a[-1])
- #   def get_sum_total(self,a):
- #       #calculate total sum
- #       sum = 0
- #       with open(self.a,"r") as fp:
- #           lines = fp.readlines()
- #       for line in lines:
- #           digit_list=[]
- #           for i in line:
- #               if i.isdigit():
- #                   digit_list.append(i)
- #           calibration=self.get_sum()        
- #           line_digit=calibration.get_sum()
- #           sum+=line_digit
- #       print(sum)
 
-#calibration=Calibration("input")
-#calculate total sum
 sum = 0
+testline=0
 with open("input","r") as fp:
     lines = fp.readlines()
 for line in lines:
+    index=0
     digit_list=[]
+    index_list=[]
     for i in line:
         if i.isdigit():
             digit_list.append(i)
-    calibration=Calibration(digit_list)        
+            index_list.append(index)
+        index+=1
+    word_list=["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    num_list=["1","2","3","4","5","6","7","8","9"]
+    i=0
+    for word in word_list:
+ 
+        word_index=line.find(word)
+        word_r_index=line.rfind(word)
+        if word_index >= 0:
+            if word_index > index_list[-1]:
+                digit_list.append(num_list[i])
+                index_list.append(word_index)
+            j=0
+            for k in index_list:
+                
+                if word_index < k:   
+                    digit_list.insert(j, num_list[i])
+                    index_list.insert(j, word_index)
+                    break
+                    
+                j+=1
+        if word_r_index > word_index:
+            if word_r_index >= 0:
+                if word_r_index > index_list[-1]:
+                    digit_list.append(num_list[i])
+                    index_list.append(word_r_index)
+            j=0
+            for k in index_list:
+                if word_r_index < k:
+                    digit_list.insert(j, num_list[i])
+                    index_list.insert(j, word_r_index)
+                    break
+                  
+                j+=1
+            
+
+        i+=1   
+    calibration=Calibration(digit_list)      
     line_digit=calibration.get_sum()
     sum+=line_digit
 print(sum)
-
-test="eightwothree"
-if "eight" in test:
-    print("three")
-if "three" in test:
-    print("eight")
-
 
